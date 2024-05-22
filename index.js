b2677d5e0a3f338dba7aa38277bd4a315c17f26a
Zeus <purslh1@unlv.nevada.edu>
2024-05-21 17:46:01 -0700

    0.0.2 added remaining props and functions

--- index.js
+++ index.js
@@ -1,12 +1,37 @@
 const { Vehicle } = require("./vehicleBaseClass");
 
 class MercurySedan extends Vehicle {
-    constructor(maximumPassengers, passengers, numberOfWheels, maximumSpeed, fuel, scheduleService){
+    constructor(make, model, year, color, mileage){
+        super(make, model, year, color, mileage);
         this.maximumPassengers = 5;
         this.passengers = 0;
         this.numberOfWheels = 4;
         this.maximumSpeed = 160;
         this.fuel = 10;
         this.scheduleService = false;
+    }
+
+    loadPassenger(num){
+        if (this.passenger + passengers < this.maxPassengers){
+            console.log("This vehicle is full!")
+            return this.passenger;
+        }
+    }
+    start(){
+        if (this.fuel > 0){
+            console.log("The engine has started successfully. There are " + this.fuel + " gallons remaining in the fuel tank.")
+            return this.started = true;
+        }
+        else{
+            console.log("The fuel tank is empty. Please refill the tank.")
+            return this.started = false;
+        }
+    }
+    scheduleService(){
+        if (this.mileage > 30000){
+            this.scheduleService = true
+            console.log("Vehicle service has been automatically scheduled.")
+            return this.scheduleService;
+        }
     }
 }
